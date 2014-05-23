0f7e27006c2958d14d3a75437802045f24679bab
Zach Dennis <zach.dennis@gmail.com>
2014-05-23 12:49:27 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# GeLo-FirmwareLibrary
+
+The static library and headers to use to interact with a Gelo beacon in configuration mode.
+
+If you are here you may want to download the Gelo over-the-air configuration app which can be found at [https://github.com/GeLoInc/GeLo-OTAC-iOS](https://github.com/GeLoInc/GeLo-OTAC-iOS).
+
+If you really want to write your own OTAC app then this is the library to use.
---
+++ include/GeLoFirmwareLibrary-public/GeLoFirmwareLibrary/GeLoDeviceService.h
@@ -0,0 +1,35 @@
+//
+//  GeLoDeviceService.h
+//  GeLoFirmwareUpdater
+//
+//  Created by Zach Dennis on 3/28/14.
+//  Copyright (c) 2014 Zach Dennis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "GeLoDevice.h"
+#import "BLEProfile.h"
+#import "GeLoAuthenticationProfile.h"
+#import "GeLoOACProfile.h"
+
+#define kGeLoDeviceAuthenticationSuccess  @"GeLoDeviceAuthenticationSuccess"
+#define kGeLoDeviceAuthenticationFailure  @"GeLoDeviceAuthenticationFailure"
+
+@interface GeLoDeviceService : NSObject<CBPeripheralDelegate>
+
+- (id) initWithDevice: (GeLoDevice *)device
+    connect: (void(^)(GeLoDevice *)) connect
+    retryConnect:(void (^)(GeLoDevice *)) retryConnect
+    disconnect: (void(^)(GeLoDevice *)) disconnect;
+- (void) connect;
+- (void) connected;
+- (void) disconnect;
+- (void) restartDevice: (void(^)(void))onRestartComplete;
+
+- (void) addAuthenticationProfile:(GeLoAuthenticationProfile *)authProfile;
+- (void) addProfile: (BLEProfile *) profile;
+
+@property (strong,nonatomic) GeLoDevice *device;
+@property (strong,nonatomic) void (^onDisconnect)(GeLoDevice *);
+
+@end
